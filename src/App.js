733eaded61ec137bf4dbb8610b3aa55d56475c6f
curogom <i_am@curogom.dev>
2021-11-15 23:37:10 +0900

    3rd Session 2 - UseState add List

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React , {useState} from "react";
 
 import NewExpense from "./components/NewExpenses/NewExpense";
 import Expense from "./components/Expenses/Expense";
 
+const DUMMY_EXPENSE = [
+    {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+    {title: 'Test1', amount: 123.45, date: new Date(2021, 3, 1)},
+    {title: 'Test2', amount: 94.12, date: new Date(2021, 3, 21)},
+    {title: 'Test3', amount: 14.00, date: new Date(2021, 4, 7)},
+    {title: 'Test4', amount: 543.21, date: new Date(2021, 4, 14)},
+];
+
 const App = () => {
-    const expenses = [
-        {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-        {title: 'Test1', amount: 123.45, date: new Date(2021, 3, 1)},
-        {title: 'Test2', amount: 94.12, date: new Date(2021, 3, 21)},
-        {title: 'Test3', amount: 14.00, date: new Date(2021, 4, 7)},
-        {title: 'Test4', amount: 543.21, date: new Date(2021, 4, 14)},
-    ]
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSE)
 
     const addExpenseHandler = (expense) => {
-        console.log('in App.js');
-        console.log(expense)
-    }
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses]
+        });
+    };
 
     return (
         <div>
