e324699031afe8507b1cb97a38d2e38fcf06d837
curogom <i_am@curogom.dev>
2021-11-15 22:55:44 +0900

    3rd Session 1 - props의 배열을 map 함수를 통해 동적 객체로 뿌리는 방법. Flutter의 build 할 때 list.map 과 유사한 형태

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Expense from "./components/Expenses/Expense";
 const App = () => {
     const expenses = [
         {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-        {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-        {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-        {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-        {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+        {title: 'Test1', amount: 123.45, date: new Date(2021, 3, 1)},
+        {title: 'Test2', amount: 94.12, date: new Date(2021, 3, 21)},
+        {title: 'Test3', amount: 14.00, date: new Date(2021, 4, 7)},
+        {title: 'Test4', amount: 543.21, date: new Date(2021, 4, 14)},
     ]
 
     const addExpenseHandler = (expense) => {
@@ -20,7 +20,7 @@ const App = () => {
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler} />
-            <Expense expenses={expenses}/>
+            <Expense items={expenses}/>
         </div>
     );
 }
